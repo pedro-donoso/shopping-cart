feaedb028483a94d393688f06f7204e01975a588
pedro <pedro.donoso.r@usach.cl>
2022-01-23 15:59:22 -0300

    agrego productos

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,12 +1,23 @@
 import React, {useState} from 'react';
 
-const ShoppingCart = () => {
+const initialCart = [
+  { id: 1, title: 'Product', description: 'Desc'},
+  { id: 2, title: 'Product #2', description: 'Desc #2'},
+]
 
-  const [cart, setCart]=useState(in)
-
+const ShoppingCart=()=>{
+  const [cart, setCart]=useState(initialCart);
   return (
     <div>
-      <h1>Shopping Cart</h1>
+
+
+
+      {cart.map(product=>(
+        <div key={product.id}>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
