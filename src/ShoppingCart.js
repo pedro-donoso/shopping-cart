7227f01cf90764eab17a5448a7e6bfa877cd09e3
pedro <pedro.donoso.r@usach.cl>
2022-01-23 17:15:52 -0300

    app funciona

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,66 +1,76 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const initialCart = [
-  { id: 1, title: 'Product', description: 'Desc'},
-  { id: 2, title: 'Product #2', description: 'Desc #2'},
-]
+  { id: 1, title: "Product", description: "Desc" },
+  { id: 2, title: "Product #2", description: "Desc #2" },
+];
 
-const ShoppingCart=()=>{
-  const [cart, setCart]=useState(initialCart);
+const ShoppingCart = () => {
+  const [cart, setCart] = useState(initialCart);
 
-  const deleteProduct=(productId)=>{
-    const changedCart = cart.filter(product=>product.id !==productId);
+  const deleteProduct = (productId) => {
+    const changedCart = cart.filter((product) => product.id !== productId);
     setCart(changedCart);
-  }
+  };
 
-  const addProduct=(newProduct)=>{
-    newProduct.id=Date.now();
+  const addProduct = (newProduct) => {
+    newProduct.id = Date.now();
 
-    const changedCart=[
-        newProduct,
-      ...cart,
-    
-    ];
+    const changedCart = [newProduct, ...cart];
     setCart(changedCart);
-  }
+  };
 
-  const updateProduct=(editProduct)=>{
-    const changedCart=cart.map(product=>(
-      product.id===editProduct.id
-      ?editProduct
-      :product
-    ));
+  const updateProduct = (editProduct) => {
+    const changedCart = cart.map((product) =>
+      product.id === editProduct.id ? editProduct : product
+    );
     setCart(changedCart);
-  }
+  };
 
   return (
     <div>
-
-      <button onClick={()=>addProduct({
-      title:" Nuevo Producto", 
-      description:"Nueva Descripción"
-      })}>
-      Add
+      <button
+        onClick={() =>
+          addProduct({
+            title: " Nuevo Producto",
+            description: "Nueva Descripción",
+          })
+        }
+      >
+        Add
       </button>
 
-      {cart.map(product=>(
+      {cart.map((product) => (
         <div key={product.id}>
-          <h2>{product.id}{product.title}</h2>
+          <h3>
+            {product.id}
+            {product.title}
+          </h3>
           <p>{product.description}</p>
-          <button onClick={()=>deleteProduct(product.id)}>
-        Delete
-      </button>
-      <button onClick={()=>updateProduct({
-      id:2,
-      title:" Producto Editado", 
-      description:"Descripción Editada"
-      })}>
-      Update
-      </button>
+          <button onClick={() => deleteProduct(product.id)}>Delete</button>
+        
+          <button
+            onClick={() =>
+              updateProduct({
+                id: product.id,
+                title: " Producto Editado",
+                description: "Descripción Editada",
+              })
+            }
+          >
+            Update
+          </button>
+            <hr/>
         </div>
       ))}
+            <div>
+            <pre>
+              {JSON.stringify(cart, null, 4)}
+            </pre>
+            </div>
+
     </div>
   );
-}
+};
 
 export default ShoppingCart;
